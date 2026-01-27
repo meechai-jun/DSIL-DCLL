ec60755768e5e43a7e316dc0096e6c9d475c6c28
navysingchai <navysingchai@gmail.com>
2026-01-27 16:32:39 +0700

    feat home: add new video section source

---
+++ JDIC/features/home/sections/video.php
@@ -0,0 +1,8 @@
+<!-- Video Section (from Oldversion) -->
+<section class="video-gallery-section section-padding ">
+    <div class="container">
+        <div class="video-box overflow-hidden rounded-4 shadow wow fadeInUp ratio ratio-16x9">
+            <iframe src="https://www.youtube.com/embed/r5fjHfvSuRA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
