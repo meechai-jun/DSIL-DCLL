366573401d4553046c7e187082571cf1b8b6997e
navysingchai <navysingchai@gmail.com>
2026-01-28 08:46:49 +0700

    added auto play feature for youtube iframe

--- JDIC/features/home/sections/video.php
+++ JDIC/features/home/sections/video.php
@@ -2,7 +2,14 @@
 <section class="video-gallery-section section-padding ">
     <div class="container">
         <div class="video-box overflow-hidden rounded-4 shadow wow fadeInUp ratio ratio-16x9">
-            <iframe src="https://www.youtube.com/embed/r5fjHfvSuRA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <!-- <iframe src="https://www.youtube.com/embed/r5fjHfvSuRA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+                <iframe
+                class="w-100 h-100"
+                src="https://www.youtube.com/embed/r5fjHfvSuRA?si=MIdofT2pB7-R9JIf&autoplay=1&mute=1"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+                style="aspect-ratio: 16/9;"> </iframe>
         </div>
     </div>
 </section>
